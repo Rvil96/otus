5d8d23816c3981b3b8985f04c5f7435741815453
Ravil <r.ishkulov96@gmail.com>
2025-01-18 15:23:09 +0300

    wip-2

--- L03-generics/src/main/java/homework/CustomerService.java
+++ L03-generics/src/main/java/homework/CustomerService.java
@@ -1,21 +1,29 @@
 package homework;
 
+import java.util.AbstractMap;
+import java.util.Comparator;
 import java.util.Map;
+import java.util.NavigableMap;
+import java.util.TreeMap;
 
-@SuppressWarnings({"java:S1186", "java:S1135", "java:S1172"}) // при выполнении ДЗ эту аннотацию надо удалить
 public class CustomerService {
-
-    // todo: 3. надо реализовать методы этого класса
-    // важно подобрать подходящую Map-у, посмотрите на редко используемые методы, они тут полезны
+    private NavigableMap<Customer, String> map = new TreeMap<>(Comparator.comparingLong(Customer::getScores));
 
     public Map.Entry<Customer, String> getSmallest() {
-        // Возможно, чтобы реализовать этот метод, потребуется посмотреть как Map.Entry сделан в jdk
-        return null; // это "заглушка, чтобы скомилировать"
+        Map.Entry<Customer, String> entry = map.firstEntry();
+        return entry == null
+                ? null
+                : new AbstractMap.SimpleEntry<>(entry.getKey().clone(), entry.getValue());
     }
 
     public Map.Entry<Customer, String> getNext(Customer customer) {
-        return null; // это "заглушка, чтобы скомилировать"
+        Map.Entry<Customer, String> entry = map.higherEntry(customer);
+        return entry == null
+                ? null
+                : new AbstractMap.SimpleEntry<>(entry.getKey().clone(), entry.getValue());
     }
 
-    public void add(Customer customer, String data) {}
+    public void add(Customer customer, String data) {
+        map.put(customer, data);
+    }
 }
